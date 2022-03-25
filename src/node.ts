a9c721dc17037e878c9f121f76077c133dda9e20
Fabio Spampinato <spampinabio@gmail.com>
2022-03-25 18:58:20 +0000

    Avoiding using Node's Buffer

--- src/node.ts
+++ src/node.ts
@@ -9,7 +9,7 @@ const makeHash = ( algorithm: 'sha1' | 'sha256' | 'sha384' | 'sha512' ) => {
 
   return ( buffer: Uint8Array | string ): Promise<string> => {
 
-    buffer = ( typeof buffer === 'string' ) ? Buffer.from ( buffer ) : buffer;
+    buffer = ( typeof buffer === 'string' ) ? new TextEncoder ().encode ( buffer ) : buffer;
 
     const hash = crypto.createHash ( algorithm );
     const hex = hash.update ( buffer ).digest ( 'hex' );
